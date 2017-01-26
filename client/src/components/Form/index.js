7624b622d1ba0a44e56469b85189a6c9d117cf65
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-01-26 13:33:08 -0600

    Define handle email change function

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -7,11 +7,16 @@ export default class Form extends React.Component {
     this.state = {name: ''};
   
     this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleNameChange(event) {
     this.setState({name: event.target.value});
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
   }
 
   handleSubmit(event) {
