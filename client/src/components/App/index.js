a841164d70a88e8e03c14c711213675cf1ef80a8
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-01-25 21:40:20 -0600

    Add home page content

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './style.css';
 
+import hi from '../../images/hi_2.png';
+import imJonathan from '../../images/im_jonathan.png';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hi</p>
+        <div className="content">
+          <img id="hi" src={hi} alt="Hi." />
+          <img id="im-jonathan" src={imJonathan} alt="I'm Jonathan."></img>
+        </div>
       </div>
     );
   }
