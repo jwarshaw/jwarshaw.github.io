5917c1ebb5430a2da8a49248c8dd17ae48a1ce5e
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-01-25 22:02:39 -0600

    Add about page skeleton and styling

---
+++ client/src/components/About/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class About extends Component {
+  // static propTypes = {}
+  // static defaultProps = {}
+  // state = {}
+
+  render() {
+    return (
+      <div className='About'>
+        <div className="about-content">
+          <p>Some stuff about me.</p>
+        </div>
+      </div>
+    );
+  }
+}
