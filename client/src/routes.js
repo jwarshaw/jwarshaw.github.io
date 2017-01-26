47ddf55aaade3ba51900f7c3012f75c0849c3f1a
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-01-25 22:09:05 -0600

    Add work page routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,11 +3,13 @@ import { Router, Route } from 'react-router';
 
 import App from './components/App';
 import About from './components/About';
+import Work from './components/Work';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App} />
  		<Route path="/about" component={About} />
+ 		<Route path="/work" component={Work} />
   </Router>
 );
 
