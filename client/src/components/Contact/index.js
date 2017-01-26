386fb24df1f913553d958909d14d55592dc2ee59
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-01-26 12:20:14 -0600

    Add contact page skeleton and stylesheet

---
+++ client/src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Contact extends Component {
+  // static propTypes = {}
+  // static defaultProps = {}
+  // state = {}
+
+  render() {
+    return (
+      <div className='Contact'>
+        <div className="contact-content">
+        </div>
+      </div>
+    );
+  }
+}
