e18afbcad68863215d1b15c05da12dd9c99aa248
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-01-30 01:16:07 -0600

    Fix typo in listen function log to console

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.post('/api/contact', (req, res) => {
 })
 
 app.listen(app.get('port'), () => {
-  console.log('Find the server at: http://localhost:${app.get('port')}/');
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
 })
