b78897f746d8fb0c0a24e7ea3ccf79a8095a6b26
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-02-01 15:57:59 -0600

    Create header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { IndexLink, Link } from 'react-router';
+import './style.css';
+
+import home from '../../images/home.png';
+import about from '../../images/about.png';
+import work from '../../images/work.png';
+import contact from '../../images/contact.png';
+
+export default class Header extends Component {
+  render() {
+    return (
+    	<ul className="header">
+    	  <li><IndexLink to="/" activeClassName="active"><img className="navBut" src={home}></img></IndexLink></li>
+        <li><Link to="/about" activeClassName="active"><img className="navBut" src={about}></img></Link></li>
+        <li><Link to="/work" activeClassName="active"><img className="navBut" src={work}></img></Link></li>
+        <li><Link to="/contact" activeClassName="active"><img className="navBut" src={contact}></img></Link></li>
+      </ul>
+    );
+  }
+}
