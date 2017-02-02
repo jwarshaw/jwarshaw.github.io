37cd6d47ea8c445d4af9f4cbf76f936eddae466e
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-02-02 15:29:00 -0600

    Debug resolve static path

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 // const nodemailer = require('nodemailer');
 // const dotenv = require('dotenv');
-// const bodyParser = require('body-parser');
 
 // dotenv.config({ silent: true});
 
@@ -24,13 +24,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-// app.use(bodyParser.json())
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+// });
 
 // app.post('/api/contact', (req, res, next) => {
 // 	console.log('hit server');
