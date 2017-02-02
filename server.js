ecfc862aae4bd7ddfa986bf78b896d0f7ff673c9
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-02-02 04:08:10 -0600

    Resolve get route path

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 // app.use(bodyParser.json())
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // app.post('/api/contact', (req, res, next) => {
