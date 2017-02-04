362fc93d8b40569f321794b721a74a5c2b3fd4ba
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-02-04 14:14:15 -0600

    Add project titles to work page

--- client/src/components/Work/index.js
+++ client/src/components/Work/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import './style.css';
 
-import car from '../../images/car.png';
+import raspberryTitle from '../../images/raspberry.png';
+import raspberry from '../../images/car.png';
+import sandboxTitle from '../../images/sandboxtitle.png';
 import sandbox from '../../images/sandbox.png';
 import calculator from '../../images/calculator.png';
+
 
 export default class Work extends Component {
   // static propTypes = {}
@@ -14,12 +17,15 @@ export default class Work extends Component {
     return (
       <div className='Work'>
         <ul className="work-list">
+          <img className='work-title' src={raspberryTitle} alt="raspberry drive"></img>
           <li className="example">
-            <img className='work-img' src={car} alt="car"></img>
+            <img className='work-img' src={raspberry} alt="car"></img>
           </li>
+          <img className='work-title' src={sandboxTitle} alt="sandbox"></img>
           <li className="example">
             <img className='work-img' src={sandbox} alt="sandbox"></img>
           </li>
+          <img className='work-title' src={sandboxTitle} alt="placeholder"></img>
           <li className="example">
             <img className='work-img' src={calculator} alt="calculator"></img>
           </li>
