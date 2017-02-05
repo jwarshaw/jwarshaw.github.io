76591be1d425093ebaf96c2aadac8ae0684f8b57
Jonathan Warshaw <jonathan.warshaw@gmail.com>
2017-02-05 14:44:00 -0600

    Implement sendgrid using mail helper class

--- server.js
+++ server.js
@@ -1,76 +1,42 @@
 const express = require('express');
 const path = require('path');
-// const bodyParser = require('body-parser');
-
-// const nodemailer = require('nodemailer');
-// const dotenv = require('dotenv');
-
-// dotenv.config({ silent: true});
-
-// const user = process.env.EMAIL_USER;
-// const pass = process.env.EMAIL_PASS;
-// const host = process.env.EMAIL_HOST;
-// const port = process.env.EMAIL_PORT;
-// const clientId = process.env.CLIENT_ID;
-// const clientSecret = process.env.CLIENT_SECRET;
-// const refreshToken = process.env.REFRESH_TOKEN;
-// const accessToken = process.env.ACCESS_TOKEN;
+const bodyParser = require(bodyParser');
 
 const app = express();
 
 app.set('port', (process.env.PORT || 3001));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 }
 
-// app.use(bodyParser.json());
-
-// app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-// app.post('/api/contact', (req, res, next) => {
-// 	console.log('hit server');
-// 	console.log('req.body...');
-//   console.log(req.body);
+app.post('/api/contact', (req, res, next) => {
+	var helper = require('sendgrid').mail;
+	var from_email = new helper.Email(req.body.email);
+	var to_email = new helper.Email(process.env.TO_EMAIL);
+	var subject = 'You have a website message from ' + req.body.name;
+	var content = new helper.Content('text/plain', req.body.message);
+	var mail = new helper.Mail(from_email, subject, to_email, content);
 
-//   var smtpConfig = {
-//     host: host,
-//     port: port,
-//     secure: true,
-//     auth: {
-//       XOAuth2: {
-//         user: 'jonathan.warshaw@gmail.com',
-//         clientId: clientId,
-//         clientSecret: clientSecret,
-//         refreshToken: refreshToken,
-//         accessToken: accessToken
-//       }
-//     }
-//   };
+	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+	var request = sg.emptyRequest({
+	  method: 'POST',
+	  path: '/v3/mail/send',
+	  body: mail.toJSON(),
+	});
 
-//   var transporter = nodemailer.createTransport(smtpConfig);
-  
-//   var mailOptions = {
-//     from: req.body.email,
-//     to: 'jonathan.warshaw@gmail.com',
-//     subject: 'Website contact form',
-//     text: req.body.message
-//   };
-
-//   transporter.sendMail(mailOptions, function(error, info){
-//     if(error){
-//     	console.log(error);
-//       return false;
-//     }else{
-//       console.log('Message sent: ' + info.response);
-//       return true;
-//     };
-//   });
-// })
+	sg.API(request, function(error, response) {
+	  console.log(response.statusCode);
+	  console.log(response.body);
+	  console.log(response.headers);
+	});
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
